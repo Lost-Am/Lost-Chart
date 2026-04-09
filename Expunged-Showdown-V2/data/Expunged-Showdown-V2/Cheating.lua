2dff7e4196399063bbe647fdf12d9764d1b6f380
Lost <103710650+Lost-Am@users.noreply.github.com>
2026-04-08 20:29:23 -0500

    Update Cheating.lua

--- Expunged-Showdown-V2/data/Expunged-Showdown-V2/Cheating.lua
+++ Expunged-Showdown-V2/data/Expunged-Showdown-V2/Cheating.lua
@@ -1,21 +1,7 @@
--- =========================
--- CONFIG
--- =========================
 local finished = false
 
--- =========================
--- START
--- =========================
-function onStartCountdown()
-    setProperty('health', 50)
-end
-
--- =========================
--- UPDATE
--- =========================
 function onUpdate(elapsed)
 
-    -- 🔴 APAGADO DEFINITIVO EN BEAT 360
     if curBeat > 410 then
         if not finished then
             finished = true
@@ -29,13 +15,11 @@ function onUpdate(elapsed)
     songPos = getSongPosition()
     local currentBeat = (songPos / 5000) * (curBpm / 60)
 
-    -- PLAYER
     noteTweenX('p0', 4, defaultPlayerStrumX0 - 50 * math.sin((currentBeat + 1) * math.pi), 0.5)
     noteTweenX('p1', 5, defaultPlayerStrumX1 - 350 + 2000 * math.sin((currentBeat + 2) * math.pi), 3)
     noteTweenX('p2', 6, defaultPlayerStrumX2 - 50 * math.sin((currentBeat + 1) * math.pi), 0.5)
     noteTweenX('p3', 7, defaultPlayerStrumX3 - 350 + 2000 * math.sin((currentBeat + 2) * math.pi), 3)
 
-    -- OPPONENT
     noteTweenX('o0', 0, defaultOpponentStrumX0 + 50 * math.sin((currentBeat + 1) * math.pi), 0.5)
     noteTweenX('o1', 1, defaultOpponentStrumX1 + 350 - 2000 * math.sin((currentBeat + 2) * math.pi), 3)
     noteTweenX('o2', 2, defaultOpponentStrumX2 + 50 * math.sin((currentBeat + 1) * math.pi), 0.5)
@@ -44,32 +28,23 @@ function onUpdate(elapsed)
     doTweenY('dadMove', 'dad', 150 - 150 * math.sin((currentBeat + 5) * math.pi), 2)
 end
 
--- =========================
--- RESET REAL (SIN TWEENS)
--- =========================
 function forceResetStrums()
 
-    -- 🔥 cancelar TODOS los tweens
     cancelTween('p0'); cancelTween('p1'); cancelTween('p2'); cancelTween('p3')
     cancelTween('o0'); cancelTween('o1'); cancelTween('o2'); cancelTween('o3')
     cancelTween('dadMove')
 
-    -- PLAYER (X + Y)
     for i = 0, 3 do
         setPropertyFromGroup('playerStrums', i, 'x', _G['defaultPlayerStrumX'..i])
         setPropertyFromGroup('playerStrums', i, 'y', getPropertyFromGroup('playerStrums', i, 'defaultY'))
     end
 
-    -- OPPONENT (X + Y)
     for i = 0, 3 do
         setPropertyFromGroup('opponentStrums', i, 'x', _G['defaultOpponentStrumX'..i])
         setPropertyFromGroup('opponentStrums', i, 'y', getPropertyFromGroup('opponentStrums', i, 'defaultY'))
     end
 end
 
--- =========================
--- DAMAGE
--- =========================
 function opponentNoteHit(id, direction, noteType, isSustainNote)
     if curBeat <= 410 then
         setProperty('health',
