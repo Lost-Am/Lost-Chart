cf2c3305a11d0b6f32f07d3a7330fced3675080a
Lost <103710650+Lost-Am@users.noreply.github.com>
2026-04-08 20:35:15 -0500

    lol

--- OtherCharts/data/ACOUSTOPHOBIA/FlyGf.lua
+++
@@ -1,9 +0,0 @@
-function onUpdate()
-	songPos = getSongPosition()
-	currentBeat = (songPos/1000)*(bpm/98)
-	setProperty("gf.scale.x",0.5)
-	setProperty("gf.scale.y",0.5)
-	setProperty("gf.y",-1000+math.sin(currentBeat*math.pi/16)*200)
-	setProperty("gf.x",-2000+math.fmod(currentBeat*100,4500))
-	setProperty("gf.angle",currentBeat*10)
-end
