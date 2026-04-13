e2ccbe14ad58f690390177e983d9669a6fad2d11
Lost <103710650+Lost-Am@users.noreply.github.com>
2026-04-13 15:56:30 -0500

    not

--- Miku/data/YTYCSTALM/fademiku.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-    setProperty('boyfriend.alpha', 0)
-end
-
-function onBeatHit()
-    if curBeat == 24 then
-        doTweenAlpha('bfFade', 'boyfriend', 1, crochet * (64-24) / 1000, 'linear')
-    end
-end
